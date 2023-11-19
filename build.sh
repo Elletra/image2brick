6ff509c5dc452569e0379cdacdb2f7590fbd006d
Electrk <Electrk@ymail.com>
2023-11-19 01:49:27 -0800

    Rename `build.sh` to `gcc-build.sh`

--- build.sh
+++
@@ -1,2 +0,0 @@
-mkdir -p ./build
-gcc ./src/main.c ./src/color.c ./src/file_path.c -o ./build/image2brick.exe
---
+++ gcc-build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+mkdir -p ./build
+gcc ./src/main.c ./src/color.c ./src/file_path.c -o ./build/image2brick
